4b41e29e5e6508b0508b983eebf6f39a56a9a864
keith.choo <cmh.keith@gmail.com>
2018-06-18 10:21:00 +0800

    node fs commit

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+var fs = require("fs");
+
 console.log('HELLO WORLD');
 console.log("HELLO AGAIN");
+
+// fs.mkdir("stuff", function(){
+//   fs.readFile("readme.txt", "utf8", function(err, data){
+//     fs.writeFile("./stuff/writeme.txt", data);
+//   })
+// })
+
+fs.unlink("./stuff/writeme.txt", function(){
+  fs.rmdir("stuff");
+})
